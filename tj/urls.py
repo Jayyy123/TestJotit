1f2586fdafb2941a0bed239bbef8c251e6fce738
Jokeseph <josephofilii@gmail.com>
2022-04-03 01:39:33 +0100

    Django Api publication for node

--- tj/urls.py
+++ tj/urls.py
@@ -22,7 +22,7 @@ from django.conf import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('taj.urls')),
+    path('/', include('taj.urls')),
     path('tu/', include('tu.urls')),
 ]
 
